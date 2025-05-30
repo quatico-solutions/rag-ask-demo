4f3a22a584aead443918a2b16e399d4a78ea2b0e
Max Albrecht <1@178.is>
2025-05-30 23:08:54 +0200

    cleanup

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Returns a greeting message.
- */
-export function hello(name: string = 'world'): string {
-  return `Hello ${name}!`;
-}
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { hello } from './index';
-
-const app = document.querySelector<HTMLDivElement>('#app');
-if (app) {
-  app.textContent = hello();
-}
